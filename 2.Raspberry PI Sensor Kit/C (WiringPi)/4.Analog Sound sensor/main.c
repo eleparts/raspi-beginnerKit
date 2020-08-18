068b6a70286214b6937712023f42a57d2a07fd56
eleparts <nyk.yeon@eleparts.co.kr>
2020-08-18 16:16:25 +0900

    name update

---
+++ 2.Raspberry PI Sensor Kit/C (WiringPi)/4.Analog Sound sensor/main.c
@@ -0,0 +1,14 @@
+/*
+* 라즈베리파이 센서 키트 Analog Sound sensor 동작 예제
+*
+* 본 예제는 온습도 센서에서 수신된 데이터를 터미널 창으로 반복해 출력하는 동작을 합니다.
+* 
+* 사용 소자 : 아날로그 사운드 센서
+* 온습도 센서 (DHT11) 
+* VCC 핀 : 3.3V | GND 핀 : GND
+* OUT :  GPIO 40번 | BCM : 21 | wiringPi : 29
+*
+* ※ 모듈 연결 시 극성에 주의해 주도록 합니다.
+*
+* 키트 정보 : https://www.eleparts.co.kr/EPXDTWR8
+*/
